4a357aa78a4110d74bd8bfc7afacb77a5c778c53
AhmedRedagh <ahmedreda.ar920@gmail.com>
2023-05-04 08:44:54 +0300

    enhancements

--- lib/views/screens/documents_send_screen/send_documents.dart
+++ lib/views/screens/documents_send_screen/send_documents.dart
@@ -1,9 +1,5 @@
-import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:salute_medical/bloc/documents_cubit/documents_cubit.dart';
 import 'package:salute_medical/views/widgets/app_bar_widget.dart';
-import 'package:salute_medical/views/widgets/documents_widget/item_uploader.dart';
 import 'package:salute_medical/views/widgets/send_documents/documents_send_profile.dart';
 
 class SendDocuments extends StatelessWidget {
@@ -12,12 +8,9 @@ class SendDocuments extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBarW(
-        name: 'Send Documents'
-      ),
+      appBar: appBarW(name: 'Send Documents'),
       body: Column(
         children: const [
-
           SendDocumentsCard(
             name: 'ID Card',
           ),
--- lib/views/widgets/send_documents/documents_send_profile.dart
+++ lib/views/widgets/send_documents/documents_send_profile.dart
@@ -3,8 +3,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:salute_medical/views/custom_widgets/custom_text.dart';
 
 class SendDocumentsCard extends StatelessWidget {
-  final String? name ;
-  const SendDocumentsCard({Key? key, this.name }) : super(key: key);
+  final String? name;
+  const SendDocumentsCard({Key? key, this.name}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -12,11 +12,12 @@ class SendDocumentsCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-             CustomText(text: name,
-            fontSize: 25,
+            CustomText(
+              text: name,
+              fontSize: 25,
               fontW: FontWeight.bold,
             ),
-            Container(
+            SizedBox(
               width: double.infinity,
               height: 100.w,
               child: Image.asset("assets/images/ic card.png"),
