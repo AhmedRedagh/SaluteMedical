5530eff6ec04bdacaebfed0238ad5dee8f3c7e06
AhmedRedagh <ahmedreda.ar920@gmail.com>
2023-05-17 14:29:16 +0300

    dio helper

---
+++ lib/config/confige.dart
@@ -0,0 +1,13 @@
+class Config {
+  /// base url
+  static const String baseUrl = "https://salutemedtrans.com/api/";
+
+  /// login url
+  static const String login = "auth/login";
+
+  /// register url
+  static const String register = "auth/register";
+
+  /// profile url
+  static const String editProfile = "edit-profile";
+}
